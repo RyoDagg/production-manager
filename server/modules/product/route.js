ce637561376ff3c48928e1f0eb06a583780a8a52
Gaddour <mannai.abdlkader@gmail.com>
2024-03-04 14:27:37 +0100

    Product #server-side POST request

--- server/modules/product/route.js
+++ server/modules/product/route.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const productController = require("./controller");
 
-router.route("/").get(productController.getAll);
+router.route("/")
+    .get(productController.getAll)
+    .post(productController.create);
 
 module.exports = router;
