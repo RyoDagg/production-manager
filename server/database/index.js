8271b530255cf70e803afc3e00c2e9c9e9ec59ba
Gaddour <mannai.abdlkader@gmail.com>
2024-03-04 11:59:09 +0100

    sequelize database connection

---
+++ server/database/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+const { host, username, password, database, dialect } = require("config").database;
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully. ✅"))
+  .catch((error) => console.error("Unable to connect to the database ❌:", error));
+
+module.exports = sequelize;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./database");
 const { server } = require("config");
 
 const app = express();
