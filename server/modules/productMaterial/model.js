a72e9627c96d5add241948d72e144375384d4e32
Gaddour <mannai.abdlkader@gmail.com>
2024-03-04 22:02:25 +0100

    reformat pivot model 'ProductMaterial' #client-side

--- server/modules/productMaterial/model.js
+++ server/modules/productMaterial/model.js
@@ -3,7 +3,7 @@ const sequelize = require("../../database");
 const Product = require("../product/model");
 const Material = require("../material/model");
 
-module.exports = sequelize.define(
+const ProductMaterial = sequelize.define(
   "ProductMaterial",
   {
     quantity: DataTypes.DECIMAL,
@@ -13,5 +13,6 @@ module.exports = sequelize.define(
   }
 );
 
-Material.belongsToMany(Product, { through: "ProductMaterial" });
-Product.belongsToMany(Material, { through: "ProductMaterial" });
+Material.belongsToMany(Product, { through: ProductMaterial });
+Product.belongsToMany(Material, { through: ProductMaterial });
+module.exports = ProductMaterial;
