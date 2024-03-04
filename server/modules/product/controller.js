8dec761399471c08a2e0f4d3e1dda29b0ccf4616
Gaddour <mannai.abdlkader@gmail.com>
2024-03-04 14:24:14 +0100

    Product #server-side POST request

--- server/modules/product/controller.js
+++ server/modules/product/controller.js
@@ -2,11 +2,21 @@ const Product = require("./model");
 
 const getAll = async (req, res) => {
   try {
-    const products = Product.findAll();
+    const products = await Product.findAll();
     res.json(products);
   } catch (error) {
     res.status(404).send(error);
   }
 };
 
-module.exports = { getAll };
+const create = async (req, res) => {
+  try {
+    console.log(req.body);
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(409).send(error);
+  }
+};
+
+module.exports = { getAll, create };
