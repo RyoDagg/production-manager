67a76403e884816d11af72465dce40d0fa18dd6d
Gaddour <mannai.abdlkader@gmail.com>
2024-03-05 16:08:19 +0100

    Update purchased material stock

--- server/modules/purchase/controller.js
+++ server/modules/purchase/controller.js
@@ -1,8 +1,11 @@
+const Material = require("../material/model");
 const Purchase = require("./model");
 
 const getAll = async (req, res) => {
   try {
-    const purchases = await Purchase.findAll();
+    const purchases = await Purchase.findAll({
+      include: { model: Material },
+    });
     res.json(purchases);
   } catch (error) {
     res.status(404).send(error);
@@ -12,6 +15,11 @@ const getAll = async (req, res) => {
 const create = async (req, res) => {
   try {
     const purchase = await Purchase.create(req.body);
+
+    // todo: Update purchased material stock
+    const material = await purchase.getMaterial();
+    material.stock += +purchase.quantity;
+    material.save();
 
     res.status(201).json(purchase);
   } catch (error) {
