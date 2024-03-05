fa6f80d678106b84ec4d9b987a375e17c420d134
Gaddour <mannai.abdlkader@gmail.com>
2024-03-05 05:04:02 +0100

    Materials select input #product-view #client-side

--- client/src/components/product/Add.jsx
+++ client/src/components/product/Add.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 const Add = ({ submit }) => {
     const [name, setName] = useState()
     const [description, setDescription] = useState()
     const [image, setImage] = useState()
     const [stock, setStock] = useState()
+    // const [material, setMaterial] = useState()
+    // const [quantity, setQuantity] = useState()
+    const [materials, setMaterials] = useState([])
+    // const [productMats, setProdMats] = useState([])
+    
+
+    useEffect(() => { fetchSelectMats() }, [])
+
+    // fetch materials for select input 
+    const fetchSelectMats = async () => {
+        try {
+            const { data } = await axios('http://127.0.0.1:3000/api/material/select')
+            setMaterials(data)
+        } catch (error) {
+            console.log('Error fetching materials!!', error);
+        }
+    }
 
     return (
         <div className="row pt-3 border text-center">
@@ -13,6 +31,7 @@ const Add = ({ submit }) => {
                     <label className="col-4 col-form-label">Name</label>
                     <div className="col-sm-8">
                         <input
+                            placeholder="Name"
                             onChange={(event) => setName(event.target.value)}
                             className="form-control" />
                     </div>
@@ -21,6 +40,7 @@ const Add = ({ submit }) => {
                     <label className="col-4 col-form-label">Description</label>
                     <div className="col-sm-8">
                         <input
+                            placeholder="Description"
                             onChange={(event) => setDescription(event.target.value)}
                             className="form-control" />
                     </div>
@@ -37,15 +57,42 @@ const Add = ({ submit }) => {
                     <label className="col-4 col-form-label">Stock</label>
                     <div className="col-sm-8">
                         <input
-                        type="number"
+                            placeholder="Stock"
+                            type="number"
                             onChange={(event) => setStock(event.target.value)}
                             className="form-control" />
+                    </div>
+                </div>
+                <div className="mb-3 row">
+                    <label className="col-4 col-form-label">Material</label>
+                    <div className="col-sm-3">
+                        <select
+                            placeholder="Material"
+                            onChange={(event) => setStock(event.target.value)}
+                            className="form-control" >
+                            <option value={null}>...</option>
+                            {materials.map(({ id, name }, i) =>
+                                <option key={i} value={id}>{name}</option>
+
+                            )}
+                        </select>
+                    </div>
+                    <div className="col-sm-3">
+                        <input
+                            placeholder="Quantity"
+                            onChange={(event) => setStock(event.target.value)}
+                            className="form-control" />
+                    </div>
+                    <div className="col-sm-2">
+                        <button
+                            onClick={() => submit({ name, description, image, stock })}
+                            className="btn btn-success">Add</button>
                     </div>
                 </div>
                 <div className="mb-3 text-right">
                     <button
                         onClick={() => submit({ name, description, image, stock })}
-                        className="btn btn-primary">Add</button>
+                        className="btn btn-success">Submit</button>
                 </div>
             </div>
         </div>
